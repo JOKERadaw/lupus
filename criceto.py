8732a67790c705db2e5a69d85182971e5e9a713a
Utente <valerioman05@gmail.com>
2026-01-16 02:26:02 +0100

    final version

--- criceto.py
+++ criceto.py
@@ -10,7 +10,7 @@ vivi=[people for people in range(giocatori)]
 #vivi=[0,1,2,3,4,5,6,7,8,9,10]
 
 def balottaggio():
-    global vivi
+    global vivi,mortodigiorno
    
     
     if len(vivi)>1:
@@ -19,6 +19,10 @@ def balottaggio():
             scelta=random.choice(vivi) 
         vivi.remove(scelta)
     
+    if scelta==indice_criceto:
+        return 1
+    else:
+        return 0
 def lupi_vivi():
     global vivi
     c=0
@@ -72,6 +76,7 @@ def guardia():
 
 criceto_morto=[0 for _ in range(giocatori)]
 tot_simulazioni=1_000_000
+morto_giorno=[0 for _ in range(giocatori)]
 for simulazione in range(tot_simulazioni):
     c=0
     vivi=[people for people in range(giocatori)]
@@ -79,6 +84,7 @@ for simulazione in range(tot_simulazioni):
     #per evitare che la veggente riguardi sempre i già visti
     indice_sguardoiniziale=random.choice([i for i in vivi if i!=indice_veggente])
     visti=[]
+    mortodigiorno=0
     while not finito:
         if indice_veggente in vivi:
             visti.append(veggente(visti))
@@ -90,15 +96,23 @@ for simulazione in range(tot_simulazioni):
         if lupi_vivi()==len(vivi)-1 and indice_criceto in vivi:
             # lupi e criceto soli
             break
-
+        morto_giorno[c]+=1
         #print(f"notte {c}:", vivi)
         #print(visti)
         if indice_criceto not in vivi: 
             criceto_morto[c]+=1
+            
             finito=True
         else:
             c+=1
-            balottaggio()
+            mortodigiorno=balottaggio()
+            if mortodigiorno:
+
+                break
+
+print("Risultati dopo",morto_giorno,"simulazioni:")
 for i in range(giocatori):
-    print(f"il criceto è morto alla notte {i} nel {criceto_morto[i]/tot_simulazioni*100}% delle volte")
+    if morto_giorno[i]>0:   
+        print(f"il criceto è morto alla notte {i} nel {criceto_morto[i]/morto_giorno[i]*100}% delle volte")
+
 # i lupi sbranano il criceto anche più volte
